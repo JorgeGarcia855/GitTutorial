37236dfb3d4ae79b59ffc9d0e848682f12d8824e
JorgeGarcia855 <82668903+JorgeGarcia855@users.noreply.github.com>
2021-08-19 19:01:56 -0500

    Update AplMain.java
    nueva variable

--- src/edu/unbosque/main/AplMain.java
+++ src/edu/unbosque/main/AplMain.java
@@ -2,6 +2,7 @@ package edu.unbosque.main;
 
 public class AplMain {
     public static void Main(String[] args){
-        System.out.println("Hello world");
+        String linea = "nuevo texto";
+        System.out.println(linea);
     }
 }
