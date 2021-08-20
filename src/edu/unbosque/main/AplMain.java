c28003995f92b2f02ebc46e4e64ba5b6931633ef
JorgeGarcia855 <jlgarciap@unbosque.edu.co>
2021-08-19 19:09:51 -0500

    variable

--- src/edu/unbosque/main/AplMain.java
+++ src/edu/unbosque/main/AplMain.java
@@ -3,6 +3,8 @@ package edu.unbosque.main;
 public class AplMain {
     public static void Main(String[] args){
         String linea = "nuevo texto";
+        int num = 6;
         System.out.println(linea);
+        System.out.println(num);
     }
 }
