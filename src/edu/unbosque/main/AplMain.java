dac092a428620d6d9dc2a72c1b03525125449c49
JorgeGarcia855 <jlgarciap@unbosque.edu.co>
2021-08-19 19:22:29 -0500

    creando develop branch

--- src/edu/unbosque/main/AplMain.java
+++ src/edu/unbosque/main/AplMain.java
@@ -3,7 +3,7 @@ package edu.unbosque.main;
 public class AplMain {
     public static void Main(String[] args){
         String linea = "nuevo texto";
-        int num = 6;
+        int num = 620;
         System.out.println(linea);
         System.out.println(num);
     }
